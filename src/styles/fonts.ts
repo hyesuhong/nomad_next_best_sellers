b4ae7a668565d0c77dbf5f8f4fb02bbeee420ff6
Hyesu <honghs95@gmail.com>
2024-11-16 19:39:04 +0900

    feat: add font setting

---
+++ src/styles/fonts.ts
@@ -0,0 +1,15 @@
+import { Glegoo, Roboto } from 'next/font/google';
+
+export const glegoo = Glegoo({
+	weight: ['400', '700'],
+	display: 'swap',
+	subsets: ['latin'],
+	variable: '--font-glegoo',
+});
+
+export const roboto = Roboto({
+	weight: ['300', '400', '500', '700'],
+	display: 'swap',
+	subsets: ['latin'],
+	variable: '--font-roboto',
+});
