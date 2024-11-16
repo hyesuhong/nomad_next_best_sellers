264e48a3266bc736d76ccb825cd1836903350140
Hyesu <honghs95@gmail.com>
2024-11-16 21:15:38 +0900

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import styles from './page.module.css';
+
+export default function About() {
+	return (
+		<main className={styles.main}>
+			<section className={styles.section}>
+				<h3>About us</h3>
+				<p>
+					Welcome to the official explorer for The New York Times Best Sellers
+					lists.
+					<br />
+					We hope you enjoy your stay!
+				</p>
+			</section>
+		</main>
+	);
+}
